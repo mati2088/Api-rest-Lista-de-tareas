1f754c2c03b2c9f56da136e26abc17341aab4ba9
matias <matiasarana46@gmail.com>
2022-12-10 16:57:39 -0300

    first commit

---
+++ src/main/java/com/app/task/tareas/context/SwaggerConfig.java
@@ -0,0 +1,42 @@
+package com.app.task.tareas.context;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Configuration
+    public class SpringFoxConfig {
+        @Bean
+        public Docket api() {
+            return new Docket(DocumentationType.SWAGGER_2)
+                    .select()
+                    .apis(RequestHandlerSelectors.any())
+                    .paths(PathSelectors.any())
+                    .build()
+                    .apiInfo(apiInfo());
+        }
+
+        private ApiInfo apiInfo() {
+            return new ApiInfo(
+                "todo api",
+                "la api rest de tid app",
+                    "v1",
+                    "terms of service",
+                    new Contact("SACAVIX tech", "www.example.com", "myaddress@company.com")
+                    ,"license if API","API license URL", Collections.emptyList());
+
+        }
+
+    }
+}
---
+++ src/test/java/com/app/task/tareas/TareasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.task.tareas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
